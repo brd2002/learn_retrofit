b8a5a242e71a4c39fe0421742b4981d3082831eb
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2024-01-20 15:59:28 +0530

    this is my first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Learn_retrofit_in_details"
+include(":app")
+ 
